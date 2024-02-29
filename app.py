05804d6b39db723c8cb49405b7bc4293fe7682cd
the-engrossed <tanmaireddy5598@gmail.com>
2024-02-29 17:57:16 -0500

    Commit2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import boto3
+import streamlit as st
 
 #Using Titan Embeddings Model to generate Embeddings
 
@@ -72,3 +73,52 @@ Assistant:
 """
 
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context",'question'])
+
+def get_response_llm(llm,vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+    )
+    answer=qa({"query":query})
+    return answer['result']
+
+def main():
+    st.set_page_config("chat PDF")
+    st.header("Chat with PDF using AWS Bedrock 💁‍♀️")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Update Or Create Vector Store:")
+
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vectorstore(docs)
+                st.success("Done")
+
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embedding)
+            llm=get_claude_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Llama2 Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embedding)
+            llm=get_llama2_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+if __name__ == "__main__":
+    main()
